113e01d0d7a168b90a2fb3b5ba2cff5d1a0e1cdf
AydenClay96 <aydenclay1996@gmail.com>
2024-09-06 21:59:48 +0100

    Updated client to send multiple messages for echo.

--- src/midi_web_app/client/echo-client.py
+++ src/midi_web_app/client/echo-client.py
@@ -5,7 +5,7 @@ PORT = 65432
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
-
-print(f"Received {data!r}")
+    while True:
+        s.sendall(b"Hello, world")
+        data = s.recv(1024)
+        print(f"Received {data!r}")
