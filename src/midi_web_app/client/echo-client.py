3844f5ea7744cf91619bb59db34e6377a0dcf75b
AydenClay96 <AydenClay1996@gmail.com>
2024-09-07 08:12:55 +0100

    changes to client to print midi message

--- src/midi_web_app/client/echo-client.py
+++ src/midi_web_app/client/echo-client.py
@@ -1,4 +1,5 @@
 import socket
+import mido
 
 HOST = "192.168.1.191"
 PORT = 65432
@@ -6,6 +7,6 @@ PORT = 65432
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     while True:
-        s.sendall(b"Hello, world")
-        data = s.recv(1024)
-        print(f"Received {data!r}")
+        with mido.open_input() as inport:
+            for msg in inport:
+                print(msg)
