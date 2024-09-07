e339f531fa7dae88e414a930cdacda0cd092b6b3
AydenClay96 <AydenClay1996@gmail.com>
2024-09-07 14:28:47 +0100

    adding initial serial checking for midi device.

--- src/midi_web_app/client/echo-client.py
+++ src/midi_web_app/client/echo-client.py
@@ -1,12 +1,25 @@
 import socket
-import mido
+import midi
+import serial
 
+# Connection IP and port.
 HOST = "192.168.1.191"
 PORT = 65432
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    while True:
-        with mido.open_input() as inport:
-            for msg in inport:
-                print(msg)
+# Get serial ports and find midi device.
+ports = ['COM%s' % (i + 1) for i in range(256)]
+result = []
+for port in ports:
+    try:
+        s = serial.Serial(port)
+        s.close()
+        result.append(port)
+    except (OSError, serial.SerialException):
+        pass
+print(result)
+
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.connect((HOST, PORT))
+#     while True:
+#         msg = "received"
+#         print(msg)
