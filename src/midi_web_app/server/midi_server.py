c639cf245deccb5a49cdd92bbcaf1a4283f2bbf2
AydenClay96 <AydenClay1996@gmail.com>
2024-09-08 14:00:39 +0100

    updated server to run and collate the client msg into a midi input in the pygame format.

--- src/midi_web_app/server/midi_server.py
+++ src/midi_web_app/server/midi_server.py
@@ -5,7 +5,7 @@ HOST = "192.168.1.191"
 PORT = 65432
 
 
-def msg_to_midi(msg: str) -> List[List[int], int]:
+def msg_to_midi(msg: str) -> List[int]:
     """Converts messages from the client to midi message in pygame.midi.Input format.
 
     Parameters
@@ -15,14 +15,15 @@ def msg_to_midi(msg: str) -> List[List[int], int]:
 
     Returns
     -------
-    List[List[int], int]
+    List:
         [[type, note, velocity], timestamp]
     """
-    type = msg[:3]
-    note = msg[3:6]
-    velocity = msg[6:9]
-    timestamp = msg[9:]
-    return [[type, note, velocity], timestamp]
+    msg_list = str(msg).split(",")
+    type, note, velocity, timestamp = msg_list
+    type = type[2:]
+    timestamp = int(timestamp[:-1])
+    midi = [int(type), int(note), int(velocity)]
+    return [midi, timestamp]
 
 
 def server_connect() -> None:
@@ -44,3 +45,9 @@ def server_connect() -> None:
                     midi = msg_to_midi(msg)
                     print(midi)
                     conn.sendall(msg)
+
+def main() -> None:
+    server_connect()
+
+if __name__ == "__main__":
+    main()
