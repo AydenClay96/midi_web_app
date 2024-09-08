7e0a7c40b326711ae2e70bb39b9ecb5f4723c158
AydenClay96 <aydenclay1996@gmail.com>
2024-09-08 08:29:03 +0100

    midi now transmits over the socket.

--- src/midi_web_app/client/echo-client.py
+++ src/midi_web_app/client/echo-client.py
@@ -1,18 +1,41 @@
 import socket
-import midi
-import win32com.client
+import pygame
+from pygame import midi
 
 # Connection IP and port.
 HOST = "192.168.1.191"
 PORT = 65432
 
-# Get USB devices attached.
-wmi = win32com.client.GetObject("winmgmts:")
-for usb in wmi.InstancesOf ("Win32_USBHub"):
-    print(usb.DeviceID)
+# Get Midi device.
+pygame.init()
+midi.init()
 
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.connect((HOST, PORT))
-#     while True:
-#         msg = "received"
-#         print(msg)
+devices = []
+for i in range(32):
+    dev = midi.get_device_info(i)
+    if dev:
+        devices.append(dev)
+
+inputs = {}
+outputs = {}
+for index, device in enumerate(devices):
+    name = device[1]
+    if device[2] == 1:
+        inputs[name] = index
+    else:
+        outputs[name] = index
+
+print(inputs)
+midi_device = midi.Input(inputs[b'Roland Digital Piano'])
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while True:
+        if midi_device.poll():
+            msg = midi_device.read(1)[0]
+            print(msg)
+            data = msg[0]
+            time = msg[1]
+            msg_str = "".join([str(e) for e in data])
+            msg_str += str(time)
+            s.sendall(str.encode(msg_str))
